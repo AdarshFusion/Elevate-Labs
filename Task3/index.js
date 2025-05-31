530eead604a436396000e19e6800b807bdbc09b0
AdarshFusion <chauhanadarsh432@gmail.com>
2025-05-31 10:25:13 +0530

    update

--- Task3/index.js
+++ Task3/index.js
@@ -1,12 +1,37 @@
 const express = require("express");
-const book = require("./MOCK_DATA.json");
+const books = require("./MOCK_DATA.json");
 
 const app = express();
 const PORT = 3000;
 
 // Routes
-app.get('/api/book', (req, res) => {
-    return res.json(book)
-})
+app.get('/api/books', (req, res) => {
+    return res.json(books)
+});
 
-app.listen(PORT,()=> console.log(`Server strated at PORT:${PORT} `))
+
+app.route('/api/books/:id')
+    .get((req, res) => {
+        const id = Number(req.params.id);
+        const book = books.find((book) => book.id === id);
+        return res.json(book);
+    })
+    .patch((req, res) => {
+        // tood edit a book with id
+        return res.json({ status: "pending" });
+    })
+    .delete((req, res) => {
+        // tood delete a book with id
+        return res.json({ status: "pending" });
+    });
+
+app.post('/api/books', (req, res) => {
+    // tood create a new book
+    const body = req.body;  
+    return res.json({ status: "pending" });
+});
+
+
+
+
+app.listen(PORT, () => console.log(`Server strated at PORT:${PORT} `))
